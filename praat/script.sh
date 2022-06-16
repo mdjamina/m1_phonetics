8dcc43d9c883fb5810c4ad582a17870bf74cab20
Amina Djarfi <m.djarfi.amina@gmail.com>
2022-06-17 00:23:52 +0200

    fix

--- praat/script.sh
+++ praat/script.sh
@@ -5,5 +5,42 @@
 #-------------------------------------------------------
 
 
+TEXTGRID=$1
 
-praat --run script_synth.praat "./input/Logatomes.TextGrid" "./input/Logatomes.wav" "./input/dico2.txt" 
+if [[ $TEXTGRID == "" ]]
+then
+    TEXTGRID="./input/Logatomes.TextGrid"
+fi
+
+
+SOUND=$2
+
+if [[ $SOUND == "" ]]
+then
+    SOUND="./input/Logatomes.wav"
+fi
+
+
+DICO=$3
+
+if [[ $DICO == "" ]]
+then
+    DICO="./input/dico2.txt"
+fi
+
+
+#Vidange des répertoires
+if [ "$(ls -A ./output)" ] 
+then 
+   rm ./output/*.wav  
+fi
+
+#praat --run script_synth_auto.praat "./input/Logatomes.TextGrid" "./input/Logatomes.wav" "./input/dico2.txt" 
+
+#praat --run script_synth_auto.praat "./input/faure.TextGrid" "./input/faure.wav" "./input/dico2.txt" 
+
+
+
+
+
+praat --run script_synth_auto.praat $TEXTGRID $SOUND $DICO
