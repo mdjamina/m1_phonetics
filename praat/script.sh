ae857172be2b841e7949fb1c69db889d6f9fbb19
Amina Djarfi <m.djarfi.amina@gmail.com>
2022-06-16 23:00:13 +0200

    v1

---
+++ praat/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#-------------------------------------------------------
+# pour lancer ce script:
+# bash script.sh <fichier Logatomes textGrid> <fichier Logatomes wav> <dictionnaire>
+#-------------------------------------------------------
+
+
+
+praat --run script_synth.praat "./input/Logatomes.TextGrid" "./input/Logatomes.wav" "./input/dico2.txt" 
