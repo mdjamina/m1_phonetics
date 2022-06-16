0eee9a0f100fdd6c8304e4465ddc956b547b6698
Amina <77275867+mdjamina@users.noreply.github.com>
2022-06-17 01:06:08 +0200

    Rattrapage (#4)

---
+++ praat/script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#-------------------------------------------------------
+# pour lancer ce script:
+# bash script.sh <fichier Logatomes textGrid> <fichier Logatomes wav> <dictionnaire>
+#-------------------------------------------------------
+
+
+TEXTGRID=$1
+
+if [[ $TEXTGRID == "" ]]
+then
+    TEXTGRID="./input/Logatomes.TextGrid"
+fi
+
+
+SOUND=$2
+
+if [[ $SOUND == "" ]]
+then
+    SOUND="./input/Logatomes.wav"
+fi
+
+
+DICO=$3
+
+if [[ $DICO == "" ]]
+then
+    DICO="./input/dico2.txt"
+fi
+
+
+#Vidange des répertoires
+if [ "$(ls -A ./output)" ] 
+then 
+   rm ./output/*.wav  
+fi
+
+#praat --run script_synth_auto.praat "./input/Logatomes.TextGrid" "./input/Logatomes.wav" "./input/dico2.txt" 
+
+#praat --run script_synth_auto.praat "./input/faure.TextGrid" "./input/faure.wav" "./input/dico2.txt" 
+
+
+
+PLAY_SOUND=0
+
+praat --run script_synth_auto.praat $TEXTGRID $SOUND $DICO $PLAY_SOUND
